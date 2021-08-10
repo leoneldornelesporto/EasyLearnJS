018b11f5e8338e4ab6e71eb187e4b67542f4472b
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-08-10 13:55:21 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './Routes';
+
+import {Template} from './components/MainComponentes';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Template>
+          <Header/>
+          <Routes/>
+          <Footer/>
+        </Template>
+      </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/components/partials/Header/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {HeaderArea} from './styles';
+import {isLogged, doLogout} from '../../../helpers/AuthHandler';
+
+function Header() {
+    let logged = isLogged();
+
+    const handleLogout = () => {
+        doLogout();
+        window.location.href = '/'; //manda para a rota home
+    }
+
+    return(
+        <HeaderArea>
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">E</span>
+                        <span className="logo-2">L</span>
+                    </Link>
+                </div>
+                <nav>
+                    <ul>
+                        {logged &&
+                        <>
+                            <li>
+                                <Link to="/post-an-ad" className="button">Anuncie seu Produto</Link>
+                            </li>
+                            <li>
+                                <Link to="/my-account">Minha Conta</Link>
+                            </li>
+                            <li>
+                                <button onClick={handleLogout}>Sair</button>
+                            </li>
+                        </>
+                        }
+                        {!logged &&
+                        <>
+                            <li>
+                                <Link to="/signin">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/signup">Cadastrar</Link>
+                            </li>
+                            <li>
+                                <Link to="/signin" className="button">Anuncie seu Produto</Link>
+                            </li>
+                        </>
+                        }
+                    </ul>
+                </nav>
+            </div>
+        </HeaderArea>
+    );
+}
+export default Header;
