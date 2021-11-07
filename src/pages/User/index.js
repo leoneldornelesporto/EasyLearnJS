79ab99240f65ea2f58bb130c6343d73d4803f04a
Leonel Dorneles Porto <leonel.porto@compasso.com.br>
2021-11-07 12:58:53 -0300

    Ajustando a entidade aula, para aceitar curso e modulo no crud.

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -65,43 +65,49 @@ const User = () => {
                     </section>
                     <section className="profile-user-guides container">
                     </section>
-                    <section className="profile-finishedCourses profile-finishedCourses--inactive profile-course-sorter">
-                        <div className="container">
-                            <h3 className="profile-title">
-                                Curso concluído
-                            </h3>
-                            <ul className="card-list profile-finishedCourses-list profile-finishedCourses-list--inactive"
-                                id="">
-                                <li className="card-list__item"
-                                    data-course-name="SOLID com Java: Princípios da programação orientada a objetos"
-                                    data-started-at="09/25/2021" data-finished-at="09/26/2021">
-                                    <div className="course-card   bootcamp-background-dark-section">
-                                        {
-                                            matriculas.map((value, index)=>{
-                                                return (
-                                                    <>
-                                                        <a className="course-card__course-link " data-recommendationid="" data-courseid=""
-                                                           data-recommendationsource="" href={"/curso_detalhe="+value.uuid}></a>
-                                                        <div className="course-card__content-wrapper">
-                                                            <div className="course-card__content">
-                                                                <img className="course-card__icon" aria-hidden="true" alt=""
-                                                                     src={value.imagemIcon}/>
-                                                                <span className="course-card__name">{value.nome}</span>
-                                                            </div>
-                                                        </div>
-                                                        <div className="course-card__other-links">
-                                                            <a className="course-card__certificate bootcamp-text-color"
-                                                               href={"/certificado="+value.uuid}>Certificado</a>
-                                                        </div>
-                                                    </>
-                                                );
-                                            })
-                                        }
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
+
+                        {
+                            matriculas===null?
+                                <></>
+                                :
+                                <section className="profile-finishedCourses profile-finishedCourses--inactive profile-course-sorter">
+                                <div className="container">
+                                    <h3 className="profile-title">
+                                        Curso concluído
+                                    </h3>
+                                    <ul className="card-list profile-finishedCourses-list profile-finishedCourses-list--inactive"
+                                        id="">
+                                        <li className="card-list__item"
+                                            data-course-name="SOLID com Java: Princípios da programação orientada a objetos"
+                                            data-started-at="09/25/2021" data-finished-at="09/26/2021">
+                                            <div className="course-card   bootcamp-background-dark-section">
+                                                {
+                                                    matriculas.map((value, index)=>{
+                                                        return (
+                                                            <>
+                                                                <a className="course-card__course-link " data-recommendationid="" data-courseid=""
+                                                                   data-recommendationsource="" href={"/curso_detalhe="+value.uuid}></a>
+                                                                <div className="course-card__content-wrapper">
+                                                                    <div className="course-card__content">
+                                                                        <img className="course-card__icon" aria-hidden="true" alt=""
+                                                                             src={value.imagemIcon}/>
+                                                                        <span className="course-card__name">{value.nome}</span>
+                                                                    </div>
+                                                                </div>
+                                                                <div className="course-card__other-links">
+                                                                    <a className="course-card__certificate bootcamp-text-color"
+                                                                       href={"/certificado="+value.uuid}>Certificado</a>
+                                                                </div>
+                                                            </>
+                                                        );
+                                                    })
+                                                }
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                </section>
+                        }
                 </main>
             )
         }
